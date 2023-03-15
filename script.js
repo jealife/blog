951c64be5834303cfbd0e6f116e782cb43e6af65
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-15 11:24:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function menuToggle() {
     var nav = document.getElementById("nav")
-    var bar = document.getElementById("body")
     var toggle = document.getElementById("toggle")
     nav.classList.toggle("active")
-    toggle.classList.toggle("active")
     toggle.classList.toggle("active")
 }
