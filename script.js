2d049644fa588b22603997ef2152913062f6c7fb
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-15 13:03:48 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function menuToggle() {
-    var nav = document.getElementById("nav")
-    var toggle = document.getElementById("toggle")
-    nav.classList.toggle("active")
-    toggle.classList.toggle("active")
-}
