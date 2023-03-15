e12cf545dfb9142c9af7ad0dae4e0b5482ccfc3d
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-15 11:14:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuToggle() {
+    var nav = document.getElementById("nav")
+    var bar = document.getElementById("body")
+    var toggle = document.getElementById("toggle")
+    nav.classList.toggle("active")
+    toggle.classList.toggle("active")
+    toggle.classList.toggle("active")
+}
